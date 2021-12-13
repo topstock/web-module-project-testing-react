493f87aa09b3acfd8f2b74df44b9fb55f69d8ab2
Phil G <fillupgalbraith@gmail.com>
2021-12-13 13:18:01 -0800

    Episode tests complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,30 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const episodeData = {
+    id: 578669,
+    number: 8,
+    runtime: 55,
+    name: "Chapter Eight: The Upside Down",
+    image: "https://www.tvmaze.com/episodes/578669/stranger-things-1x08-chapter-eight-the-upside-down.",
+    summary: "Jim convinces Brenner to let him and Joyce enter the Upside Down to find Will... unaware that Brenner plans to recover Jane no matter what it takes. Meanwhile, Nancy and Jonathan prepare to trap the monster at the Byers house, but receive an unexpected visitor.",
+    season: 1
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={episodeData} />)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    const episode = { ...episodeData, summary: 'Lorem ipsum dolor sept.'}
+    render(<Episode episode={episode} />)
+    const summaryProp = screen.getByText(/Lorem ipsum dolor sept/ig);
+    expect(summaryProp).toBeInTheDocument();
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders default image when image is not defined", ()=>{
+    let episode = { ...episodeData, image: ''};
+    render(<Episode episode={episode} />);
+    const imageLoaded = screen.queryByRole('img');
+    expect(imageLoaded.src).toBe('https://i.ibb.co/2FsfXqM/stranger-things.png');
+});
