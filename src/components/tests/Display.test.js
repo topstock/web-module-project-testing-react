024f8e9a862f5615cf2ffa974ec19b580a5112c9
Phil G <fillupgalbraith@gmail.com>
2021-12-13 22:56:53 -0800

    all display tests pass

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,7 +4,9 @@ import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
-
+import fetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
+console.log({fetchShow});
 const showData = {
     name: "Lorem ipsum",
     summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -32,19 +34,132 @@ test('renders without errors with no props', ()=>{
 });
 
 test('renders Show component when the button is clicked ', async ()=>{
-    const mockFetchShow = jest.fn( () => {
-
-    })
-    render(<Display show={showData} selectedSeason='none' />);
+  
+    fetchShow.mockResolvedValueOnce({
+        name: "Lorem ipsum",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        seasons: [
+            { 
+                id: 1234,
+                name: "Lorem ipsum Season 1",
+                episodes: []
+            },
+            {
+                id: 2345,
+                name: "Lorem ipsum Season 2",
+                episodes: []
+            },
+            {
+                id: 3456,
+                name: "Lorem ipsum Season 3",
+                episodes: []
+            }
+        ]
+    });
+    render(<Display show={null} selectedSeason='none' />);
+   
     const button = screen.getByRole('button');
     userEvent.click(button);
-    const showDescription = screen.findByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua/i);
-    // const showDescription = await waitFor(() => {
-    //     return screen.queryByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua/i)
-    // });
-   expect(showDescription).toHave(value, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+
+    const heading = await screen.findByRole('heading');
+
+    expect(heading).toBeInTheDocument();
 });
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', async ()=>{
+    fetchShow.mockResolvedValueOnce({
+        name: "Lorem ipsum",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        seasons: [
+            { 
+                id: 1234,
+                name: "Lorem ipsum Season 1",
+                episodes: []
+            },
+            {
+                id: 2345,
+                name: "Lorem ipsum Season 2",
+                episodes: []
+            },
+            {
+                id: 3456,
+                name: "Lorem ipsum Season 3",
+                episodes: []
+            }
+        ]
+    });
+    render(<Display  show={null} selectedSeason='none' />);
+   
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const option1 = await screen.findByText(/Lorem ipsum Season 1/i);
+    const option2 = await screen.findByText(/Lorem ipsum Season 2/i);
+    const option3 = await screen.findByText(/Lorem ipsum Season 3/i);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    expect(option1).toBeInTheDocument();
+    expect(option2).toBeInTheDocument();
+    expect(option3).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async ()=>{
+    fetchShow.mockResolvedValueOnce({
+        name: "Lorem ipsum",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        seasons: [
+            { 
+                id: 1234,
+                name: "Lorem ipsum Season 1",
+                episodes: []
+            },
+            {
+                id: 2345,
+                name: "Lorem ipsum Season 2",
+                episodes: []
+            },
+            {
+                id: 3456,
+                name: "Lorem ipsum Season 3",
+                episodes: []
+            }
+        ]
+    });
+    render(<Display show={null} selectedSeason='none' />);
+   
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const options = await screen.findAllByTestId('season-option');
+
+    expect(options.length).toBe(3);
+});
+
+
+test('runs optional function', async()=>{
+    fetchShow.mockResolvedValueOnce({
+        name: "Lorem ipsum",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        seasons: [
+            { 
+                id: 1234,
+                name: "Lorem ipsum Season 1",
+                episodes: []
+            },
+            {
+                id: 2345,
+                name: "Lorem ipsum Season 2",
+                episodes: []
+            },
+            {
+                id: 3456,
+                name: "Lorem ipsum Season 3",
+                episodes: []
+            }
+        ]
+    });
+ const mockDisplayFunc = jest.fn();
+ render(<Display displayFunc={mockDisplayFunc} show={null} selectedSeason='none'/>);
+ 
+ const button = screen.getByRole('button');
+ userEvent.click(button);
+ await screen.findAllByTestId('season-option');
+ expect(mockDisplayFunc).toBeCalled();
+});
